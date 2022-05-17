e764f8d4639b7eeac348d9c020fb7a43c7c94685
Sedef Cakmak <cakmaksedef@gmail.com>
2022-05-17 16:51:10 +0300

    working on animations

--- src/scenes/CharacterSelect.js
+++ src/scenes/CharacterSelect.js
@@ -12,14 +12,6 @@ export default class CharacterSelect extends Phaser.Scene {
       "assets/spritesheet_jumper.png",
       "assets/spritesheet_jumper.xml"
     );
-
-    this.anims.create({
-      key: "dazzle",
-      frameRate: 10,
-      repeat: -1,
-      frames: "jumper",
-      // frames: this.anims.generateFrameNumbers("jumper", { start: 5, end: 8 }),
-    });
   }
 
   create() {
@@ -39,8 +31,8 @@ export default class CharacterSelect extends Phaser.Scene {
     this.input.keyboard.once("keydown-SPACE", this.handleContinue, this);
     bunny1 = this.add.sprite(340, 430, "jumper", "bunny1_stand.png");
     bunny2 = this.add.sprite(140, 430, "jumper", "bunny2_stand.png");
-    this.add.sprite(240, 430).play("dazzle");
 
+    // console.log(this.textures.get("jumper").frames);
     // bunny2.anims.add("testing", "bunny_stand.png", [0, 1]);
     bunny2.setInteractive();
     // this.anims.create(
@@ -60,10 +52,22 @@ export default class CharacterSelect extends Phaser.Scene {
   }
 
   update() {
-    bunny2.on("pointerover", function () {}, this);
+    bunny2.on(
+      "pointerover",
+      function () {
+        console.log("hover");
+      },
+      this
+    );
 
     bunny2.on("pointerout", function () {
       console.log("out");
     });
+
+    // if (bunny2.pointerOver()) {
+    //   bunny2.alpha = 1;
+    // } else {
+    //   bunny2.alpha = 0.5;
+    // }
   }
 }
